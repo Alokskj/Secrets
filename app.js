fabea27860d0e1cb5c8b484ec354274515d5cdc9
alokskj <alokskj14@gmail.com>
2023-02-16 18:41:31 +0530

    before Oauth

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const port = process.env.PORT || 3000;
 
@@ -12,74 +14,92 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(
+  session({
+    secret: "thisisoursecret",
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1/UserDB")
-.then("connedted to the server")
-.catch((err)=>{console.log(err)})
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://127.0.0.1/UserDB")
+  .then("connedted to the server")
+  .catch((err) => {
+    console.log(err);
+  });
 
 const userSchema = new mongoose.Schema({
-    email : {
-        type: String,
-        unique : true
-    },
-    password : String,
-})
+  username: String,
+  password: String,
+});
+userSchema.plugin(passportLocalMongoose);
 
+const user = mongoose.model("users", userSchema);
 
-userSchema.plugin(encrypt, {secret : process.env.SECRET_KEY, encryptedFeild : ["password"]})
+passport.use(user.createStrategy());
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
 
-const user = mongoose.model('users', userSchema,)
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+app.get("/logout", function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
 
+});
 
-app.get("/", (req, res)=>{
-    res.render("home");
-})
-app.get("/login", (req, res)=>{
-    res.render("login");
-})
-app.get("/register", (req, res)=>{
-    res.render("register");
-})
+app.post("/register", async (req, res) => {
+  user.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
 
-app.post("/register", (req, res)=>{
-    const userEmail = req.body.username;
-    const userPass = req.body.password;
-
-    const newUser = new user({
-        email : userEmail,
-        password : userPass,
-    })
-
-    newUser.save((err)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("secrets")
-        }
-    })
-})
-
-app.post("/login", (req, res)=>{
-    const userEmail = req.body.username;
-    const userPass = req.body.password;
-    user.findOne({email : userEmail}, (err, foundUser)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            if(foundUser.password === userPass){
-                res.render("secrets")
-            }
-            else{
-                res.send("User not found")
-            }
-        }
-    })
-})
-
-
+app.post("/login", async (req, res) => {
+  const User = new user({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(User, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
